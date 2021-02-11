3447c9ff809348a70613d1f0b8d6a5d807d271c0
Natashalexx <78483838+Natashalexx@users.noreply.github.com>
2021-02-11 20:10:26 +0300

    Add files via upload

---
+++ Homework_3.2.py
@@ -0,0 +1,5 @@
+#Задача 2
+def my_func(name, surname, byear, city, email, phone):
+    print(name, surname, byear, city, email, phone)
+
+my_func(name= 'Natalya', surname='Putintseva', byear=1985, city='Voronezh', email='email', phone='5060')

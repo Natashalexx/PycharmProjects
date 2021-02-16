58e009f909c28f3ca0c5567867fbe052ebf045c0
Natashalexx <78483838+Natashalexx@users.noreply.github.com>
2021-02-16 15:19:56 +0300

    Add files via upload

---
+++ Home_work_4.1.py
@@ -0,0 +1,13 @@
+def simple_calc():
+    x = float(input('Введите количество отработанных часов : '))
+    y = float(input('Введите сумму оплаты труда за 1 час : '))
+    c = float(input('Укажите размер премии - '))
+    pay = x * y
+    return pay + c
+print(f'Размер заработной платы составил: {simple_calc() }')
+
+
+
+
+
+
---
+++ Home_work_4.4.py
@@ -0,0 +1,5 @@
+
+my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+my_new_list = [el for num, el in enumerate(my_list) if my_list[num - 1] < my_list[num]]
+print(f'Исходный список {my_list}')
+print(f'Новый список {my_new_list}')
